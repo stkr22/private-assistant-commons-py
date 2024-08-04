e10d5498e72a802835d1e1e7f8c772f7a0f070ac
stkr22 <stkr22@github.com>
2024-08-04 09:52:31 +0000

    reverting some copier changes - side effect of sync;

---
+++ src/private_assistant_commons/__init__.py
@@ -0,0 +1,13 @@
+from .base_skill import BaseSkill
+from .messages import ClientRequest, IntentAnalysisResult, NumberAnalysisResult, SkillCertainty, SkillRegistration
+from .skill_config import SkillConfig
+
+__all__ = [
+    "BaseSkill",
+    "ClientRequest",
+    "IntentAnalysisResult",
+    "NumberAnalysisResult",
+    "SkillCertainty",
+    "SkillRegistration",
+    "SkillConfig",
+]
